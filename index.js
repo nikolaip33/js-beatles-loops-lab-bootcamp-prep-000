7a851c629677edd97427b33584b11b8cd565a1fe
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-14 21:19:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  for(let i = 1; i < facts.length; i++) {
+  for(let i = 0; i < facts.length; i++) {
     facts[i] = `${facts[i]}!!!`
   }
   return facts
